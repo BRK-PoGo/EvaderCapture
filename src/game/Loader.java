2f415a5211ff024062d61537bd933c3c9cadd95f
BRK-PoGo <tjwally29@gmail.com>
2017-06-20 15:38:12 +0200

    Fuck all of this

---
+++ src/game/Loader.java
@@ -0,0 +1,30 @@
+package game;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class Loader {
+	public Graph LoadGraph(JFrame parent) {
+		Graph graph = null;
+		JFileChooser c = new JFileChooser();
+		int rVal = c.showOpenDialog(parent);
+	    if (rVal == JFileChooser.APPROVE_OPTION) {
+	        String fileName = c.getSelectedFile().getName() + ".ser";
+	        String pathName = c.getCurrentDirectory().toString();
+	        String dirName = pathName + "\\" + fileName;
+	        try (ObjectInputStream ois =
+					new ObjectInputStream(new FileInputStream(dirName))) {
+
+				graph = (Graph) ois.readObject();
+				System.out.println("Done");
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+	    }
+	    return graph;
+	}
+}

819e703f855c3603ad3e80673e7882e649c86b10
Raffaele Piccini <lele.raffa97@gmail.com>
2017-06-22 11:25:15 +0200

    push

--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -211,7 +211,7 @@ public class GameFrame extends JFrame {
 					if(ent instanceof Evader){
 						ent.setSpeed(Integer.parseInt(textField.getText()));
 						if(rdbtnRandomEvad.isSelected())///set algorithm
-							ent.setAlgorithm(new Run(gamePanel.graph));
+							ent.setAlgorithm(new Random(gamePanel.graph));
 						else if(rdbtnComandEvad.isSelected()){
 						ent.setAlgorithm(new Run(gamePanel.graph));				
 						}

909a4ab22d47e820d1dfddf44f87c6a10e25f3a4
Raffaele Piccini <lele.raffa97@gmail.com>
2017-06-25 13:52:47 +0200

    ,

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -71,7 +71,8 @@ public class GamePanel extends JPanel {
 			for(int i=0; i<graph.getNodeGrid().length; i++) {
 	            for (int j=0; j<graph.getNodeGrid()[i].length; j++) {
       	if(graph.getNodeGrid()[i][j].getDirtyCleanValue()>0){
-	            		g.setColor(new Color(95,158,160,(float)graph.getNodeGrid()[i][j].getDirtyCleanValue()));
+      					g.drawString(Double.toString(graph.getNodeGrid()[i][j].getDirtyCleanValue()), graph.getNodeGrid()[i][j].rectangle.x-1, graph.getNodeGrid()[i][j].rectangle.y);
+	            		g.setColor(Color.cyan);
 	            		g.fillRect(graph.getNodeGrid()[i][j].rectangle.x, graph.getNodeGrid()[i][j].rectangle.y, graph.getNodeGrid()[i][j].rectangle.width, graph.getNodeGrid()[i][j].rectangle.height);
 	            		g.setColor(Color.gray);
 	            	}if (graph.getNodeGrid()[i][j].getVision()) {
